bcb0111698f49cebc2ee192400ef992a5de965fc
alejandro <alejandro.12w@gmail.com>
2025-10-13 19:33:52 -0700

    TRADE DONE

--- app/page.js
+++ app/page.js
@@ -1,6 +1,30 @@
 export default function Home() {
   return (
-    <div className="bg-white dark:bg-gray-900 min-h-screen">
+    <div className="min-h-screen bg-gray-900 w-full">
+      {/* Title */}
+      <div
+        className="flex justify-center pt-[60px] mb-[60px]"
+        style={{
+          width: "292.07px",
+          height: "76.8px",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
+        <h1
+          className="text-white text-center align-middle uppercase"
+          style={{
+            fontFamily: "var(--font-abhaya-libre), serif",
+            fontWeight: 800,
+            fontSize: "48px",
+            lineHeight: "76.8px",
+            letterSpacing: "11px",
+          }}
+        >
+          HOME
+        </h1>
+      </div>
+
       <div className="container mx-auto px-4 py-8"></div>
     </div>
   );
